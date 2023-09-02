436cc564da0d3594d230c2edac528e68e9754f45
Nganga Ngaruiya <107641141+NgangaNgaruiya001@users.noreply.github.com>
2023-09-02 23:53:01 +0300

    Update settings.py

--- student_database/settings.py
+++ student_database/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'aged_student_database',
         'USER':'postgres',
         'PASSWORD':'Ab422450398',
-        # 'HOST':'',
+        'HOST':'',
         'PORT':'5432',
     }
 }
