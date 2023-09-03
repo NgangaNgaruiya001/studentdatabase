ffa8f69a9ca78b5cd401b27122ba8a816dede5a0
Nganga Ngaruiya <107641141+NgangaNgaruiya001@users.noreply.github.com>
2023-09-03 08:38:40 +0300

    Update settings.py

--- student_database/settings.py
+++ student_database/settings.py
@@ -79,17 +79,23 @@ WSGI_APPLICATION = 'student_database.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'aged_student_database',
+#         'USER':'postgres',
+#         'PASSWORD':'Ab422450398',
+#         'HOST':'postgre',
+#         'PORT':'5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aged_student_database',
-        'USER':'postgres',
-        'PASSWORD':'Ab422450398',
-        'HOST':'postgre',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
